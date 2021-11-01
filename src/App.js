f5fb0b7020f228978c85b870455316d3a850c1c2
kavinragul <93508775+kavinragul@users.noreply.github.com>
2021-10-31 23:19:09 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,54 @@
+
+import './App.css';
+
+import React, {Header} from './component/header/header' ;
+function App() {
+  return (
+    <div>
+      {/* <Header />
+     <h2>HTML Table</h2>
+
+<table>
+  <tr>
+    <th>Company</th>
+    <th>Contact</th>
+    <th>state</th>
+  </tr>
+  <tr>
+    <td>HCL</td>
+    <td>kavin kumar</td>
+    <td>tamil nadu</td>
+  </tr>
+  <tr>
+    <td>micrsoft</td>
+    <td>bala</td>
+    <td>delhi</td>
+  </tr>
+  <tr>
+    <td>capgemini</td>
+    <td>surya</td>
+    <td>karnataka</td>
+  </tr>
+  <tr>
+    <td>wipro</td>
+    <td>vijay</td>
+    <td>andhra pradesh</td>
+  </tr>
+  <tr>
+    <td>CTS</td>
+    <td>selvaraj</td>
+    <td>karnataka</td>
+  </tr>
+  <tr>
+    <td>TCS</td>
+    <td>haris</td>
+    <td>tamil nadu</td>
+  </tr>
+</table> */}
+<Header/>
+  
+    </div>
+  );
+}
+
+export default App;
